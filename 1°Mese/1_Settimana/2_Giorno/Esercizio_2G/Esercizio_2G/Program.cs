65b65ec283a4fef93cd7796e1a5097d92689f792
mirko-amoroso <mirkoamoroso599@gmail.com>
2024-06-18 16:29:32 +0200

    Test...

---
+++ 1°Mese/1_Settimana/2_Giorno/Esercizio_2G/Esercizio_2G/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using esercizio_2G;
+
+namespace esercizio_2G
+{
+    internal class Program
+    {
+        static void Main(String[] args)
+        {
+            Persona persona_1 = new Persona("giacomo", "abiuso", 22);
+
+            persona_1.getNome();
+            persona_1.getCognome();
+            persona_1.getEta();
+
+            persona_1.getDetails();
+
+            Console.WriteLine("il suo nome è: " + persona_1.Nome);
+        }
+    }
+}
---
+++ 1°Mese/1_Settimana/2_Giorno/Esercizio_2G/Esercizio_2G/persona.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using esercizio_2G;
+
+namespace esercizio_2G
+{
+    internal class Persona
+    {
+        string nome;
+        string cognome;
+        int eta;
+
+        public string Nome
+        {
+            get { return nome; }
+        } 
+        public string Cognome
+        {
+            get { return cognome; }
+        } 
+        public int Eta
+        {
+            get { return eta; }
+        }
+
+
+        public Persona(string nome, string cognome, int eta)
+        {
+            this.nome = nome;
+            this.cognome = cognome;
+            this.eta = eta;
+        }
+
+        public void getNome()
+        {
+            Console.WriteLine("il suo nome è: " + this.nome);
+        }
+
+        public void getCognome()
+        {
+            Console.WriteLine("il suo cognome è: " + this.cognome);
+        }
+
+        public void getEta()
+        {
+            Console.WriteLine("la sua età è: " + this.eta);
+        }  
+        public void getDetails()
+        {
+            Console.WriteLine("le sue info sono: \nnome " + this.nome + "; \n" + "cognome " + this.cognome + "; \n" + "eta " + this.eta + ";");
+        }
+    }
+}
