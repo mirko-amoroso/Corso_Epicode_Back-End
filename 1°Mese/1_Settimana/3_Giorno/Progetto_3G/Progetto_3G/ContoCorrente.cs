c409a5d49ec9472c6ce103f8e683d5797bae9bde
mirko-amoroso <mirkoamoroso599@gmail.com>
2024-06-19 15:52:23 +0200

    esercizio 3G

---
+++ 1°Mese/1_Settimana/3_Giorno/Progetto_3G/Progetto_3G/ContoCorrente.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progetto_3G
+{
+    internal class ContoCorrente
+    {
+        private int Conto;
+
+        public ContoCorrente(int conto)
+        {
+            if (conto >= 1000)
+            {
+                Conto = conto;
+                Console.WriteLine($"conto creato con successo: sono stati aggiunti {conto}");
+            }
+            else
+            {
+                conto = 1000;
+                Console.WriteLine(
+                    "il tuo conto non può essere inferiore a 1000€ sono stati accreditati 1000€"
+                );
+            }
+        }
+
+        public void prelievo(int soldi)
+        {
+            if (this.Conto >= soldi)
+            {
+                this.Conto -= soldi;
+                Console.WriteLine($"ti sono rimasti {this.Conto}€");
+            }
+            else
+            {
+                Console.WriteLine($"nel tuo conto hai {this.Conto}€");
+            }
+        }
+
+        public void versamento(int soldi)
+        {
+            this.Conto += soldi;
+            Console.WriteLine($"nel tuo conto hai {this.Conto}€");
+        }
+    }
+}
---
+++ 1°Mese/1_Settimana/3_Giorno/Progetto_3G/Progetto_3G/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Progetto_3G
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            ContoCorrente conto_giacomino = new ContoCorrente(1400);
+            conto_giacomino.prelievo(400);
+            conto_giacomino.versamento(600);
+        }
+    }
+}
