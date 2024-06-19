3a0bb76d42f90ce8d80b9a3b031604bac75ec332
mirko-amoroso <mirkoamoroso599@gmail.com>
2024-06-19 16:29:17 +0200

    esercizio 3G

---
+++ 1°Mese/1_Settimana/3_Giorno/esercizio_3G/esercizio_3G/Program.cs
@@ -0,0 +1,14 @@
+﻿using esercizio_3G;
+
+namespace Progetto_3G
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Esercizio array = new Esercizio(["MirKo", "GIAcomo", "sarA", "aNDReE"]);
+            array.Cerca("giacomo");
+        }
+    }
+}
---
+++ 1°Mese/1_Settimana/3_Giorno/esercizio_3G/esercizio_3G/esercizo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace esercizio_3G
+{
+    internal class Esercizio
+    {
+        private string[] Lista;
+
+        public Esercizio(string[] lista)
+        {
+            Lista = lista;
+        }
+
+
+        public void Cerca(string parola)
+        {
+            parola = parola.ToLower();
+            for (int i = 0; i < Lista.Length; i++)
+            {
+                Lista[i] = Lista[i].ToLower();
+            }
+            foreach (string s in Lista) 
+            {
+                if (s == parola)
+                {
+                    Console.WriteLine($"si è presente la parola {parola}");
+                }
+            }
+        }
+    }
+}
